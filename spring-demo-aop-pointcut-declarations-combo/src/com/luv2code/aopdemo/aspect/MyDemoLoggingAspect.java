f2e9237e9d9f5de91323356335b97c30f1411877
Abhishek <abhishek10men@gmail.com>
2021-07-06 13:41:12 +0530

    PointCut declaration combo implementation : && or || in pointcut annotation.

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,40 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+	//this is where we add all of our related advices for logging
+	
+	//@Before Advice
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	public void forDaoPackage() {}
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	public void getter() {}
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	public void setter() {}
+	
+	@Pointcut("forDaoPackage() && !getter() && !setter()")
+	public void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n---> Executing before advice on addAccount()");
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		
+		System.out.println("---> performing api analytics");
+		
+	}
+	
+}
